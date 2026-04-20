5b5ded449e6bd9a602193b480c9240d7fe52f387
Varun Chadha <vichadha@protonmail.com>
2026-04-19 19:57:43 -0500

    Move SolverState to new file with companion object

--- scala/src/main/scala/sudoku/Solver.scala
+++ scala/src/main/scala/sudoku/Solver.scala
@@ -1,17 +1,5 @@
 package sudoku
 
-/** Case class to be propagated through recursive calls.
-  *
-  * @param blankCells
-  *   List of locations of blank cells
-  * @param candidates
-  *   Map of location to possible candidates for that location
-  */
-case class SolverState(
-    blankCells: List[Location],
-    candidates: Map[Location, Candidates]
-)
-
 // Output of solver
 // Map of blank locations to filled number
 type SolutionMap = Map[Location, Filled]
@@ -36,7 +24,7 @@ object Solution {
       case Right(board) => board
 
     // Initialize solution set
-    val initialSolverState = buildSolverState(cellBoard)
+    val initialSolverState = SolverState.from(cellBoard)
 
     // Solve board
     val solution = solve(initialSolverState)
@@ -49,41 +37,6 @@ object Solution {
         )
       case Some(soln) =>
         soln.foreach { case (loc, Filled(d)) => board(loc.row)(loc.col) = d }
-  }
-
-  private def buildSolverState(board: Array[Array[Cell]]): SolverState = {
-    // Compute set of numbers in rows, cols, and sub boxes
-    // These are only used to get possible values for the initial blank cells
-    // Afterwards we simply update the candidates for each blank cell and never
-    // need to recompute the row, col, or subbox sets
-    val rowSets = board.map(row => CellHelpers.toFilledSet(row)).toVector
-    val colSets =
-      board.transpose.map(col => CellHelpers.toFilledSet(col)).toVector
-    val subBoxSets =
-      Utils.getSubBoxCells(board).map(CellHelpers.toFilledSet).toVector
-
-    val blankCells =
-      board.zipWithIndex.flatMap { case (row, rowIndex) =>
-        row.zipWithIndex.collect {
-          case (cell, colIndex) if cell == Blank => Location(rowIndex, colIndex)
-        }
-      }.toList
-
-    // Create a map of possible values for each blank cell
-    val candidates = blankCells.map { location =>
-      val possibleValues =
-        Filled.validFilled
-          -- rowSets(location.row)
-          -- colSets(location.col)
-          -- subBoxSets(location.subBoxIndex)
-
-      location -> Candidates(possibleValues)
-    }.toMap
-
-    SolverState(
-      blankCells = blankCells,
-      candidates = candidates
-    )
   }
 
   private def solve(solverState: SolverState): Option[SolutionMap] =
---
+++ scala/src/main/scala/sudoku/SolverState.scala
@@ -0,0 +1,50 @@
+package sudoku
+
+/** Case class to be propagated through recursive calls.
+  *
+  * @param blankCells
+  *   List of locations of blank cells
+  * @param candidates
+  *   Map of location to possible candidates for that location
+  */
+case class SolverState(
+    blankCells: List[Location],
+    candidates: Map[Location, Candidates]
+)
+
+object SolverState {
+  def from(board: Array[Array[Cell]]): SolverState = {
+    // Compute set of numbers in rows, cols, and sub boxes
+    // These are only used to get possible values for the initial blank cells
+    // Afterwards we simply update the candidates for each blank cell and never
+    // need to recompute the row, col, or subbox sets
+    val rowSets = board.map(row => CellHelpers.toFilledSet(row)).toVector
+    val colSets =
+      board.transpose.map(col => CellHelpers.toFilledSet(col)).toVector
+    val subBoxSets =
+      Utils.getSubBoxCells(board).map(CellHelpers.toFilledSet).toVector
+
+    val blankCells =
+      board.zipWithIndex.flatMap { case (row, rowIndex) =>
+        row.zipWithIndex.collect {
+          case (cell, colIndex) if cell == Blank => Location(rowIndex, colIndex)
+        }
+      }.toList
+
+    // Create a map of possible values for each blank cell
+    val candidates = blankCells.map { location =>
+      val possibleValues =
+        Filled.validFilled
+          -- rowSets(location.row)
+          -- colSets(location.col)
+          -- subBoxSets(location.subBoxIndex)
+
+      location -> Candidates(possibleValues)
+    }.toMap
+
+    SolverState(
+      blankCells = blankCells,
+      candidates = candidates
+    )
+  }
+}
